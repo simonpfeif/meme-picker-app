2d7f309826ff827068d4385f1c977bc9c57c5074
Simon Pfeiffer <simonhpfeiffer@gmail.com>
2026-01-05 21:32:08 -0600

    Added event listener for highlighting

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e){
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+ 
+
 function getEmotionsArray(cats){
-    const emotionsArray = []
+    const emotionsArray = []    
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-            emotionsArray.push(emotion)
+            if (!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
         }
     }
     return emotionsArray
@@ -14,10 +23,20 @@ function getEmotionsArray(cats){
 
 
 function renderEmotionsRadios(cats){
+        
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+        radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`
     }
     emotionRadios.innerHTML = radioItems
 }
