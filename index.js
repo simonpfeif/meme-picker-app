84bc882244ae84668a8a92aad40a02b58eb78658
Simon Pfeiffer <simonhpfeiffer@gmail.com>
2026-01-06 20:33:04 -0600

    Removed persistent highlighting

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const emotionRadios = document.getElementById('emotion-radios')
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
 function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
  
