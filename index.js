b520b2a6e38cc5bc22a68a1e32e9c106cab1831a
Simon Pfeiffer <simonhpfeiffer@gmail.com>
2026-01-08 19:58:05 -0600

    Adding select from cats data logic

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
+getImageBtn.addEventListener('click', getMatchingCatsArray)
+
 function highlightCheckedOption(e){
     const radios = document.getElementsByClassName('radio')
-    
     for (let radio of radios){
         radio.classList.remove('highlight')
     }
-
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
- 
+
+
+function getMatchingCatsArray(){     
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const isGif = gifsOnlyOption.checked
+        
+        const matchingCatsArray = catsData.filter(function(cat){
+            return cat.emotionTags.includes(selectedEmotion)
+        })
+        
+        console.log(matchingCatsArray)
+    }  
+}
 
 function getEmotionsArray(cats){
     const emotionsArray = []    
@@ -48,3 +63,7 @@ function renderEmotionsRadios(cats){
 }
 
 renderEmotionsRadios(catsData)
+
+
+
+
